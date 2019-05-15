d6e78757d0adb24ce710984548369d9a527a143a
Sendy Halim <sendyhalim93@gmail.com>
2019-05-16 02:04:05 +0700

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Argo",
   dependencies: [
-    .Package(url: "https://github.com/thoughtbot/Runes.git", majorVersion: 4)
+    .package(url: "https://github.com/thoughtbot/Runes.git", .upToNextMajor(from: "4.2.1"))
   ]
 )
