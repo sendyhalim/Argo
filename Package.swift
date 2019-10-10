a134917128a0b03302aa50bea28b2868f4b958e9
Sendy Halim <sendyhalim93@gmail.com>
2019-10-11 01:55:19 +0700

    Set package target and product

--- Package.swift
+++ Package.swift
@@ -4,7 +4,17 @@ import PackageDescription
 
 let package = Package(
   name: "Argo",
+  products: [
+    .library(name: "Argo", targets: ["Argo"])
+  ],
   dependencies: [
     .package(url: "https://github.com/thoughtbot/Runes.git", .upToNextMajor(from: "5.0.0"))
+  ],
+  targets: [
+    .target(
+      name: "Argo",
+      dependencies: ["Runes"],
+      path: "./Sources/"
+    )
   ]
 )
