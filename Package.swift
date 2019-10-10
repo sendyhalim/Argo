7594f36542486b6c53add02b26253295216beb2b
Sendy Halim <sendyhalim93@gmail.com>
2019-10-11 01:47:46 +0700

    Fix version label

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ import PackageDescription
 let package = Package(
   name: "Argo",
   dependencies: [
-    .package(url: "https://github.com/thoughtbot/Runes.git", majorVersion: 4)
+    .package(url: "https://github.com/thoughtbot/Runes.git", .upToNextMajor(from: "5.0.0"))
   ]
 )
